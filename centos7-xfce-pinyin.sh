e639e267f6609502d49f55c2b0b2a24c3179d8bc
Pan Wenbin <panwenbin@gmail.com>
2019-01-19 22:42:36 +0800

    去掉X Window system

--- centos7-xfce-pinyin.sh
+++ centos7-xfce-pinyin.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 yum -y install epel-release
-yum -y groupinstall "X Window system"
 yum -y groupinstall "Xfce"
 systemctl set-default graphical.target
 yum -y install wqy-microhei-fonts ibus-libpinyin im-chooser
