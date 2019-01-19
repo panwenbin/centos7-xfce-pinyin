321f0031d566e042f0dabf002b9dc626c64c8c3d
Pan Wenbin <panwenbin@gmail.com>
2019-01-19 23:47:55 +0800

    修改时区

--- centos7-xfce-pinyin.sh
+++ centos7-xfce-pinyin.sh
@@ -4,4 +4,5 @@ yum -y groupinstall "Xfce" "X Window system"
 systemctl set-default graphical.target
 yum -y install wqy-microhei-fonts ibus-libpinyin im-chooser
 localectl set-locale LANG=zh_CN.UTF8
+timedatectl set-timezone Asia/Shanghai
 systemctl isolate graphical.target
