54cf6eeb4e6414d7931c8cc750210d4e6e82c661
Pan Wenbin <panwenbin@gmail.com>
2019-01-19 23:07:51 +0800

    把X Window sysmtem放到Xfce后面

--- centos7-xfce-pinyin.sh
+++ centos7-xfce-pinyin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum -y install epel-release
-yum -y groupinstall "Xfce"
+yum -y groupinstall "Xfce" "X Window system"
 systemctl set-default graphical.target
 yum -y install wqy-microhei-fonts ibus-libpinyin im-chooser
 localectl set-locale LANG=zh_CN.UTF8
