7976f891793f55125276bc3f0293343faf5cd550
Pan Wenbin <panwenbin@gmail.com>
2019-02-17 00:48:53 +0800

    输入法框架改为fcitx，精简要安装的包

--- README.md
+++ README.md
@@ -10,17 +10,18 @@
 ```
 
 ### 可能需要的配置
+- 改用lightdm后，界面的语言设置要手动选一下，不知道为什么
 - 如果输入法没有出来，手动执行im-chooser命令
 ```
 $ im-chooser
 ```
-选择`使用IBus`
+选择`使用FCITX`
 
 - 自动选中输入法。在/etc/bashrc或者~/.bashrc中加入如下内容，重新登录
 ```
-export XMODIFIERS=@im=ibus
-export GTK_IM_MODULE=ibus
-export QT_IM_MODULE=ibus
+export XMODIFIERS=@im=fcitx
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
 ```
 
 - 如果默认面板弄坏了，手动删除当前用户的Xfce配置，然后重启
--- centos7-xfce-pinyin.sh
+++ centos7-xfce-pinyin.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 yum -y install epel-release
-yum -y groupinstall "Xfce" "X Window system"
-systemctl set-default graphical.target
-yum -y install wqy-microhei-fonts ibus-libpinyin im-chooser
+yum -y install lightdm fcitx fcitx-pinyin fcitx-configtool wqy-microhei-fonts im-chooser xfce4-appfinder xfce4-panel xfce4-power-manager xfce4-pulseaudio-plugin xfce4-session xfce4-session-engines xfce4-settings xfce4-terminal xfdesktop thunar thunar-volman
+yum -y groupinstall "X Window system"
 localectl set-locale LANG=zh_CN.UTF8
 timedatectl set-timezone Asia/Shanghai
+systemctl set-default graphical.target
 systemctl isolate graphical.target
